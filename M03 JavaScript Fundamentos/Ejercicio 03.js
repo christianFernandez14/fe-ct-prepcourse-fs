52583c65ccabd1c71f2dd585d60a6d8fc9a5784d
Christian Fernandez <christian14@gmail.com>
2023-06-02 23:47:00 -0400

    Solución de Homework modúlo 03 v.02

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -2,68 +2,74 @@
 /*3️⃣ EJERCICIO 03 3️⃣*/
 
 function sonIguales(x, y) {
-   // Retorna true si "x" e "y" son iguales.
-   // De lo contrario, retorna false.
-   // Tu código:
 
-   if (x === y) {
-      return true;
-   }else {
-      return false;
-   }
+   // Solucion #2:
+   return true ? x === y : false;
+
+   // Solucion #1:
+   // if (x === y) {
+   //    return true;
+   // }else {
+   //    return false;
+   // }
 }
 
 function tienenMismaLongitud(str1, str2) {
-   // Retorna true si los dos strings tienen la misma longitud.
-   // De lo contrario, retorna false.
-   // Tu código:
 
-   if (str1.length === str2.length) {
-      return true;
-   }else {
-      return false;
-   }
+   // Solucion #2:
+      return true ? str1.length === str2.length : false;
+
+   // Solucion #1:
+   // if (str1.length === str2.length) {
+   //    return true;
+   // }else {
+   //    return false;
+   // }
 }
 
 function menosQueNoventa(num) {
-   // Retorna true si el argumento "num" es menor que noventa.
-   // De lo contrario, retorna false.
-   // Tu código:
 
-   if (num < 90 ) {
-      return true;
-   }else return false;
+   // Solución #2:
+   return (num < 90);
+
+   // Solución #1:
+   // if (num < 90 ) {
+   //    return true;
+   // }else return false;
 }
 
 function mayorQueCincuenta(num) {
-   // Retorna true si el argumento "num" es mayor que cincuenta.
-   // De lo contrario, retorna false.
-   // Tu código:
+   
+   // Solución #2:
+   return num > 50;
 
-   if (num > 50) {
-      return true;
-   }else return false;
+   // Solución #1:
+   // if (num > 50) {
+   //    return true;
+   // }else return false;
    
 }
 
 function esPar(num) {
-   // Retorna true si "num" es par.
-   // De lo contrario, retorna false.
-   // Tu código:
 
-   if (num % 2 === 0){
-      return true;
-   } else return false;
-}
-
-function esImpar(num) {
-   // Retorna true si "num" es impar.
-   // De lo contrario, retorna false.
-   // Tu código:
+   // Solución #2:
+   return num % 2 === 0;
    
-   if (num % 2 !== 0){
-      return true;
-   } else return false;
+   // Solución #1:
+   // if (num % 2 === 0){
+      //    return true;
+      // } else return false;
+   }
+   
+function esImpar(num) {
+
+   // Solución #2:
+   return num % 2 !== 0;
+   
+   // Solución #1:
+   // if (num % 2 !== 0){
+   //    return true;
+   // } else return false;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
